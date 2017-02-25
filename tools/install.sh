86bc5b866a2b5fdc5417da1f7f910dfe64572997
akeil <alex@akeil.net>
2017-02-25 12:54:23 +0100

    ignore

--- tools/install.sh
+++ tools/install.sh
@@ -2,7 +2,7 @@
 cd /home/akeil/code/steamwatch
 python setup.py sdist
 
-pip=/home/akeil/.venvs/remindme/bin/pip
+#pip=/home/akeil/.venvs/remindme/bin/pip
 dist=file:///home/akeil/code/steamwatch/dist
-$pip install --upgrade --force-reinstall --pre --no-deps --no-index --find-links "$dist" steamwatch
-#$pip install --upgrade --force-reinstall --pre --find-links "$dist" steamwatch
+#pip install --user --upgrade --force-reinstall --pre --no-deps --no-index --find-links "$dist" steamwatch
+pip install --user --upgrade --force-reinstall --pre --find-links "$dist" steamwatch
