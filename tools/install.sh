c42187a651446f04cf7fed855bcaaee70e2253da
akeil <alex@akeil.net>
2017-02-25 12:58:13 +0100

    remove tools/ from repo

--- tools/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/akeil/code/steamwatch
-python setup.py sdist
-
-#pip=/home/akeil/.venvs/remindme/bin/pip
-dist=file:///home/akeil/code/steamwatch/dist
-#pip install --user --upgrade --force-reinstall --pre --no-deps --no-index --find-links "$dist" steamwatch
-pip install --user --upgrade --force-reinstall --pre --find-links "$dist" steamwatch
